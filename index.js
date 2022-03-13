54f9d88cdd37d3798fc76b5af6a343d396de8612
badgeth-dao <81835284+badgeth-dao@users.noreply.github.com>
2022-03-13 13:08:29 -0700

    120px test

--- index.js
+++ index.js
@@ -11,8 +11,8 @@ svgBadgeFileNames.forEach((badge) => {
   const compressedPath = `./compressed/${badge}.json`;
   const allFileContents = fs.readFileSync(badgePath, "utf-8");
   allFileContents.split(/\r?\n/).forEach((line) => {
-    const cxMatch = line.match(/cx='([0-9]+)'/);
-    const cyMatch = line.match(/cx='([0-9]+)'/);
+    const cxMatch = line.match(/cx='([0-9\.]+)'/);
+    const cyMatch = line.match(/cy='([0-9\.]+)'/);
     const fillMatch = line.match(/style='fill: rgb\(([0-9, ]+)\);'/);
 
     const cx = cxMatch ? Number(cxMatch[1]) : null;
@@ -59,25 +59,24 @@ svgBadgeFileNames.forEach((badge) => {
     JSON.parse(allFileContents));
 
   const chunkedData = chunk(compressedData.data, 3);
+
   chunkedData.forEach((line, i) => {
-    console.log(line);
     const cx = cxPositions[line[0]];
     const cy = cyPositions[line[1]];
     const color = colors[line[2]];
     circles.push(
-      `<circle cx='${cx}' cy='${cy}' r='5' fill='rgba(${color})' />`
+      `<circle cx='${cx}' cy='${cy}' r='1.25' fill='rgba(${color})' />`
     );
   });
 
   const svg = `
   <svg xmlns='http://www.w3.org/2000/svg' version='1.1' width='120' height='120'>
   <g stroke-width='0'>
+  <rect x='0' y='0' width='120' height='120' style='fill: rgb(0,0,0);' />
     ${circles.join("\n")}
   </g>
     </svg>
   `;
-
-  console.log(circles);
 
   fs.writeFileSync(testPath, svg);
 });
